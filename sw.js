48d1e089fb4717763b1ff5f22469a90bc99bdc36
santiaj8 <83044210+santiaj8@users.noreply.github.com>
2026-02-12 14:09:10 -0500

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,16 @@
+const CACHE = "interval-routines-v1";
+const ASSETS = ["./", "./index.html", "./manifest.webmanifest"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)));
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.map((k) => (k === CACHE ? null : caches.delete(k)))))
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request)));
+});

b07e74127443a6d1482c6eb1078921918507a884
santiaj8 <83044210+santiaj8@users.noreply.github.com>
2026-02-12 14:28:37 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "interval-routines-v1";
+const CACHE = "interval-routines-v2";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest"];
 
 self.addEventListener("install", (event) => {
